a551294ce04cd5c296259b188aded334f356abde
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-05-20 17:18:51 -0400

    calculate volume of a cuboid

---
+++ cubicVolume.js
@@ -0,0 +1,9 @@
+//calculate the volume of a cuboid with length width and height. easy peesy. took maybe 2 mins
+
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // your code here
+    return length*width*height;
+  }
+}
